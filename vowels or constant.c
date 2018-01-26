72ee952288393a80eec2020c6c895515bf5e536d
gokulgopal <35801031+gokulgopal@users.noreply.github.com>
2018-01-26 14:20:13 +0530

    Create vowels or constant.c

---
+++ vowels or constant.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+void main()
+{
+char b[6]={'a','e','i','o','u'};
+char g;
+scanf("%c",&g);
+if(b[0]==g||b[1]==g||b[2]==g||b[3]==g||b[4]==g)
+printf("\n the given alphabet is vowel");
+else
+printf("\n the given alphbet is consonant");
+}
